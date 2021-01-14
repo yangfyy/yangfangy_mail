4b3784cb20b33b6786d80988176100fe5cfc5cf0
yangfyy <1092562954@qq.com>
2021-01-14 11:15:21 +0800

    vue脚手架

---
+++ src/router/routers.js
@@ -0,0 +1,23 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+//
+// Vue.use(Router)
+// export default [
+//   {
+//     path: '/login',
+//     name: 'login',
+//     meta: {
+//       title: '登录',
+//       hideInMenu: true
+//     },
+//     component: () => import('@/common/login.vue')
+//   }, {
+//     path: '/home',
+//     name: 'index',
+//     meta: {
+//       title: '主页',
+//       hideInMenu: true
+//     },
+//     component: () => import('@/common/index.vue')
+//   }
+// ]
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const glob = require('glob')
+
+function getPages () {
+//得到当前项目中我们创建得对应页面的对应的入口js文件
+}
